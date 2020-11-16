4d3c495ef92260aa9200b08bc39b7c5461af8668
singhkunal2050 <singhkunal2050@gmail.com>
2020-11-16 19:47:11 +0530

    api working

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
+const pup = require('puppeteer')
+// import pup from './node_modules/puppeteer'
+
 const express = require('express')
 
 const app = express()
 const port = process.env.PORT || 2324 
 
 app.get('/' , (req , res) =>{
-  res.send('<h2> Heyya </h2>')
+  res.send(`<h2> Heyya </h2>`)
+
 })
 
 app.get('/api/:domname', function (req, res) {
+  getDomainAvailability(req.params.domname)
   res.send('Request for :: ' + req.params.domname)
 })
 
@@ -16,3 +21,27 @@ app.listen(port , ()=>{
 })
 
 
+
+async function getDomainAvailability(domain){
+  let url = `https://in.godaddy.com/domainsearch/find?checkAvail=1&domainToCheck=${domain}`
+  const browser = await pup.launch()
+  try{
+    const page = await browser.newPage()
+    await page.goto(url)
+    // await sleep(3000);
+    // getting elem by xpath .. rwturns an array whose first item is stored using destructuring 
+    const [elem] = await page.$x('//*[@id="search-app"]/div/div/div[2]/div/div/div/div/div[2]/div[1]/div/span')
+    const op = await elem.getProperty('innerText');
+    const parsedop = await op.jsonValue();
+    
+    if(parsedop.includes('is taken'))
+      console.log('Gone :(');
+    else
+      console.log(`Yayyyyy :} ${url}`);
+    browser.close();
+  }catch(e){
+    // console.error(`Exception Occured :: ${e}`)
+    console.log('Something Went Wrong!');
+    browser.close();
+  }
+}
--- scraper.js
+++ scraper.js
@@ -27,4 +27,4 @@ async function getDomainAvailability(domain){
 
 }
 
-getDomainAvailability('domainforksr');
+// getDomainAvailability('domainforksr');
